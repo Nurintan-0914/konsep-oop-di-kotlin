b613e62fc0dc9cd198bd01665e03dc8621bda41e
Nurintan <nurintann0914@gmail.com>
2024-09-24 11:18:19 +0700

    Add InheritanceA.kt

---
+++ src/main/kotlin/InheritanceA.kt
@@ -0,0 +1,20 @@
+//Parents class
+open class Computer(val name: String,
+                        val brand: String){
+
+}
+
+//chld class (initializes the parent class)
+class Laptop(name: String,
+            brand: String,
+            val batteryLife: Double) : Computer(name, brand){
+                fun info(){
+                    println("Name : $name")
+                    println("Brand : $brand")
+                    println("Baterry Life : $batteryLife")
+                }
+            }
+fun main(){
+    val myLaptop = Laptop("acer aspire 4738", "Acer", 2.5)
+    println(myLaptop.info())
+}

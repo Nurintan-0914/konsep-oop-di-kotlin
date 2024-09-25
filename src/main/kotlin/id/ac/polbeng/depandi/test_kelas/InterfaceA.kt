db0cb1520f6f915a63416c162ff6a652b3a1eae1
Nurintan <nurintann0914@gmail.com>
2024-09-25 21:45:42 +0700

    Add InterfaceA.kt

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/InterfaceA.kt
@@ -0,0 +1,23 @@
+//package id.ac.polbeng.depandi.test_kelas
+//
+//interface Myinterface {
+//    var str: String
+//    fun demo()
+//    fun func(){
+//        println("hello")
+//    }
+//}
+//
+//class MyClass: MyInterface{
+//    override var str: String = "rekayasa perangkat Lunak"
+//    override fun demo(){
+//        println("Demo Function")
+//    }
+//}
+//
+//fun main(){
+//    val obj = MyClass()
+//    obj.demo()
+//    obj.func()
+//    println(obj.str)
+//}
